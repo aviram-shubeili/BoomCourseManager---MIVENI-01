757cba6dd464191aa727e5cd3010129744a5a525
avira <aviramshovali@gmail.com>
2020-11-23 12:47:02 +0200

    Hey

--- Courses.h
+++ Courses.h
@@ -4,17 +4,17 @@
 #include "library.h"
 
 class Course{
-    private:
+private:
     std::shared_ptr<Node_classes>* classes;
     std::shared_ptr<Node_views>* num_views;
-    public:
+public:
     Course(int num_classes, int course_id);
     ~Course();
 
     std::shared_ptr<Node_classes> getClass(int class_id);
     std::shared_ptr<Node_views> getViews(int class_id);
     StatusType addViews(int class_id);
-    
+
 }
 
 #endif
