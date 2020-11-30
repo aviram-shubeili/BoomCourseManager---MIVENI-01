e821700607e87141a93c1dea992c96833514a231
avira <aviramshovali@gmail.com>
2020-11-30 21:58:58 +0200

    sda

--- Auxiliaries.h
+++ Auxiliaries.h
@@ -2,13 +2,17 @@
 #ifndef BOOM_AUXILIARIES_H
 #define BOOM_AUXILIARIES_H
 
+#include <memory>
 #include "BoomExceptions.h"
+#include <cassert>
+#include <iostream>
 /**
  * return true if key <= 0, false otherwise
  * @param key
  * @return
  */
 bool isInvalid(int key);
+
 
 int max(int a,int b);
 
